c7b692be49320765179e756c66cc44b4affe6a9a
Bailey <dabailey107@gmail.com>
2020-08-29 18:21:07 +0900

    Add project files.

---
+++ DCS-Module-Hider/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DCS_Module_Hider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1_selectTheModules = new System.Windows.Forms.Label();
+            this.button2_confirmAndExport = new System.Windows.Forms.Button();
+            this.button1_selectLua = new System.Windows.Forms.Button();
+            this.textBox1_luaLocation = new System.Windows.Forms.TextBox();
+            this.checkedListBox1_modules = new System.Windows.Forms.CheckedListBox();
+            this.button3_deleteLua = new System.Windows.Forms.Button();
+            this.button4_helpReadmee = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1_selectTheModules
+            // 
+            this.label1_selectTheModules.AutoSize = true;
+            this.label1_selectTheModules.Location = new System.Drawing.Point(12, 9);
+            this.label1_selectTheModules.Name = "label1_selectTheModules";
+            this.label1_selectTheModules.Size = new System.Drawing.Size(203, 13);
+            this.label1_selectTheModules.TabIndex = 0;
+            this.label1_selectTheModules.Text = "Select the DCS modules you want to see:";
+            this.label1_selectTheModules.Click += new System.EventHandler(this.label1_selectTheModules_Click);
+            // 
+            // button2_confirmAndExport
+            // 
+            this.button2_confirmAndExport.Location = new System.Drawing.Point(15, 409);
+            this.button2_confirmAndExport.Name = "button2_confirmAndExport";
+            this.button2_confirmAndExport.Size = new System.Drawing.Size(141, 23);
+            this.button2_confirmAndExport.TabIndex = 1;
+            this.button2_confirmAndExport.Text = "Confirm and Export";
+            this.button2_confirmAndExport.UseVisualStyleBackColor = true;
+            this.button2_confirmAndExport.Click += new System.EventHandler(this.button2_confirmAndExport_Click);
+            // 
+            // button1_selectLua
+            // 
+            this.button1_selectLua.Location = new System.Drawing.Point(15, 380);
+            this.button1_selectLua.Name = "button1_selectLua";
+            this.button1_selectLua.Size = new System.Drawing.Size(141, 23);
+            this.button1_selectLua.TabIndex = 2;
+            this.button1_selectLua.Text = "Select DCS Config Folder";
+            this.button1_selectLua.UseVisualStyleBackColor = true;
+            this.button1_selectLua.Click += new System.EventHandler(this.button1_selectLua_Click);
+            // 
+            // textBox1_luaLocation
+            // 
+            this.textBox1_luaLocation.Location = new System.Drawing.Point(15, 354);
+            this.textBox1_luaLocation.Name = "textBox1_luaLocation";
+            this.textBox1_luaLocation.ReadOnly = true;
+            this.textBox1_luaLocation.Size = new System.Drawing.Size(267, 20);
+            this.textBox1_luaLocation.TabIndex = 3;
+            // 
+            // checkedListBox1_modules
+            // 
+            this.checkedListBox1_modules.FormattingEnabled = true;
+            this.checkedListBox1_modules.Location = new System.Drawing.Point(15, 25);
+            this.checkedListBox1_modules.Name = "checkedListBox1_modules";
+            this.checkedListBox1_modules.Size = new System.Drawing.Size(267, 319);
+            this.checkedListBox1_modules.TabIndex = 4;
+            // 
+            // button3_deleteLua
+            // 
+            this.button3_deleteLua.Location = new System.Drawing.Point(162, 380);
+            this.button3_deleteLua.Name = "button3_deleteLua";
+            this.button3_deleteLua.Size = new System.Drawing.Size(120, 23);
+            this.button3_deleteLua.TabIndex = 5;
+            this.button3_deleteLua.Text = "Unhide DCS Modules";
+            this.button3_deleteLua.UseVisualStyleBackColor = true;
+            this.button3_deleteLua.Click += new System.EventHandler(this.button3_deleteLua_Click);
+            // 
+            // button4_helpReadmee
+            // 
+            this.button4_helpReadmee.Location = new System.Drawing.Point(162, 409);
+            this.button4_helpReadmee.Name = "button4_helpReadmee";
+            this.button4_helpReadmee.Size = new System.Drawing.Size(120, 23);
+            this.button4_helpReadmee.TabIndex = 6;
+            this.button4_helpReadmee.Text = "Help / Readmee";
+            this.button4_helpReadmee.UseVisualStyleBackColor = true;
+            this.button4_helpReadmee.Click += new System.EventHandler(this.button4_helpReadmee_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 442);
+            this.Controls.Add(this.button4_helpReadmee);
+            this.Controls.Add(this.button3_deleteLua);
+            this.Controls.Add(this.checkedListBox1_modules);
+            this.Controls.Add(this.textBox1_luaLocation);
+            this.Controls.Add(this.button1_selectLua);
+            this.Controls.Add(this.button2_confirmAndExport);
+            this.Controls.Add(this.label1_selectTheModules);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DMoHi v1 by Bailey";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1_selectTheModules;
+        private System.Windows.Forms.Button button2_confirmAndExport;
+        private System.Windows.Forms.Button button1_selectLua;
+        private System.Windows.Forms.TextBox textBox1_luaLocation;
+        private System.Windows.Forms.CheckedListBox checkedListBox1_modules;
+        private System.Windows.Forms.Button button3_deleteLua;
+        private System.Windows.Forms.Button button4_helpReadmee;
+    }
+}
+
---
+++ DCS-Module-Hider/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Welcome to DCS Module Hider (DMoHi). This program will enable you to select which aircraft
+ * you want to display or hide on the main menu of DCS. It works for modules you have not
+ * purchased, modules you have purchased, and selected mods. Enjoy!
+ * 
+ * To add more modules to this program all you have to do as add their 'pluginEnabled.lua'
+ * specific name to the "string[] modules' list.
+ */
+
+namespace DCS_Module_Hider
+{
+    public partial class Form1 : Form
+    {
+
+        string appPath = Application.StartupPath;//gets the path of were the utility us running
+        string fullPath_pluginsEnabledLua;
+        string correctConfigFolderCheck = ("Config");
+        string pathOfConfigFolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Sets up the initial objects in the CheckedListBox.
+            string[] modules = { "P-47D-30 by Eagle Dynamics", "MiG-19P by RAZBAM" , "MiG-21Bis by Magnitude 3 LLC",
+                "NS430_Mi-8MT", "F/A-18C","Su-27 Flanker by Eagle Dynamics", "L-39C", "Su-33 Flanker by Eagle Dynamics", 
+                "VAICOM PRO by Hollywood_315", "Tacview by Raia Software", "Nevada", "FW-190D9 Dora by Eagle Dynamics",
+            "NS430_L-39C", "Combined Arms by Eagle Dynamics", "Christen Eagle II by Magnitude 3 LLC", "SU-57 PAK FA by CUBANACE SIMULATIONS",
+            "MiG-29 Fulcrum by Eagle Dynamics", "Yak-52 by Eagle Dynamics", "MiG-15bis by Belsimtek",
+            "NS430", "TheChannel", "F-15C", "Su-25A by Eagle Dynamics", "Normandy", "Fw 190 A-8 by Eagle Dynamics", "C-101 Aviojet",
+            "I-16 by OctopusG", "AV-8B N/A by RAZBAM Sims", "A-10A by Eagle Dynamics", "Bf 109 K-4 by Eagle Dynamics",
+            "Flaming Cliffs by Eagle Dynamics","A-10C Warthog by Eagle Dynamics" ,"AJS37 Viggen by Heatblur Simulations",
+            "F-16C bl.50", "VARS Pylons 2019 by GrinnelliDesigns","Syria" ,
+            "Mi-8MTV2 Hip by Belsimtek","MB-339PAN Original model by FTV" , "M-2000C by RAZBAM Sims" ,
+            "F-14B by Heatblur Simulations","Supercarrier" ,"PersianGulf" ,"Spitfire LF Mk. IX by Eagle Dynamics" ,"DCS-SRS" ,
+            "F-5E by Belsimtek","Su-25T by Eagle Dynamics" ,"UH-1H Huey by Belsimtek" , "JF-17 by Deka Ironwork Simulations",
+            "F-86F Sabre by Belsimtek","TF-51D Mustang by Eagle Dynamics" ,"A-4E-C" ,"SA342 Gazelle by Polychop-Simulations" ,
+            "Caucasus","Ka-50 Black Shark by Eagle Dynamics" ,"Edge540 FM by Aero" ,"P-51D Mustang by Eagle Dynamics","jsAvionics"};
+            checkedListBox1_modules.Items.AddRange(modules);
+            checkedListBox1_modules.CheckOnClick = true;
+            checkedListBox1_modules.Sorted = true;
+
+            //if there is a settings file, load it
+            if (File.Exists(appPath + @"/DCS-Module-Hider-Settings/DMoHi-UserSettings.txt"))
+            {
+                //MessageBox.Show("Loading...");
+                System.IO.StreamReader file = new System.IO.StreamReader(appPath + @"/DCS-Module-Hider-Settings/DMoHi-UserSettings.txt");
+                //read the file line by line. Assumes the lines are properly formated via the saveSettings method
+                //if no custom resoution was saved, the lines will be blank, which is ok because the fields will also be blank as a result
+                fullPath_pluginsEnabledLua = file.ReadLine();
+                file.Close();
+                //set the textboxes to the data that was read
+                textBox1_luaLocation.Text = fullPath_pluginsEnabledLua;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string moduleName;
+        private void button2_confirmAndExport_Click(object sender, EventArgs e)
+        {
+            //check that the lua path has been set
+            if (String.IsNullOrEmpty(fullPath_pluginsEnabledLua))
+            //if one of the path is empty or null, the user didnt have them set
+            {
+                MessageBox.Show("It looks like you did not select the correct pluginsEnabled.lua. Please select your pluginsEnabled.lua and try again.");
+                return;
+            }
+            else
+            {
+                saveLuaLocation();
+                exportLua();
+                
+            }
+        }
+
+        private void saveLuaLocation()
+            {
+            //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-write-to-a-text-file
+            string[] exportLines = { fullPath_pluginsEnabledLua };
+            Directory.CreateDirectory(appPath + @"/DCS-Module-Hider-Settings");
+            File.WriteAllLines(appPath + @"/DCS-Module-Hider-Settings/DMoHi-UserSettings.txt", exportLines);
+        }
+
+        private void exportLua()
+        {
+            //export everything
+            Directory.CreateDirectory(appPath + @"/DCS-Module-Hider-Settings");
+
+            //https://docs.microsoft.com/en-us/dotnet/api/system.io.streamwriter?redirectedfrom=MSDN&view=netcore-3.1
+            //using (StreamWriter sw = new StreamWriter(appPath + @"/DCS-Module-Hider-Settings/DMoHi-Export.txt"))
+            using (StreamWriter sw = new StreamWriter(fullPath_pluginsEnabledLua))
+            {
+                //begins the file with necessarry DCS stuff
+                sw.WriteLine("pluginsEnabled = ");
+                sw.WriteLine("{");
+                //goes through the list and identifies the checked boxes
+                //then it puts the text in a variable and prints the formated line
+                //then it moves to the next checked box
+                foreach (object itemChecked in checkedListBox1_modules.CheckedItems)
+                {
+                    moduleName = itemChecked.ToString();
+                    sw.WriteLine("    [\"" + moduleName + "\"] = true,");
+                    //MessageBox.Show(moduleName);
+                }
+
+                //https://stackoverflow.com/questions/30240481/how-to-do-a-loop-on-all-unchecked-items-from-checkedlistbox-c
+                //similar to what it does to the checked items, but for the unchecked items.
+                IEnumerable<object> notChecked = (from object item in checkedListBox1_modules.Items
+                                                  where !checkedListBox1_modules.CheckedItems.Contains(item)
+                                                  select item);
+                foreach (object item in notChecked)
+                {
+                    // your code
+                    moduleName = item.ToString();
+                    sw.WriteLine("    [\"" + moduleName + "\"] = false,");
+                }
+
+                sw.WriteLine("} -- end of pluginsEnabled");//caps off the end of the file
+            }
+            MessageBox.Show("exported to '" + fullPath_pluginsEnabledLua + "'. If that does not " +
+                        "look correct, please try again.");
+        }
+
+        private void button1_selectLua_Click(object sender, EventArgs e)
+        {
+            //https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.folderbrowserdialog.-ctor?view=netcore-3.1#System_Windows_Forms_FolderBrowserDialog__ctor
+            //uses a folder selector because the user may not have the lua if
+            //they have never used the module manager to disable a module
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+
+            folderDlg.ShowNewFolderButton = false;
+            folderDlg.Description = ("Select your DCS Config Folder then click 'OK' (Hint: C:\\Users\\YOURNAME\\Saved Games\\DCS\\Config");
+            DialogResult result = folderDlg.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                //do a check
+                pathOfConfigFolder = folderDlg.SelectedPath;
+                if (pathOfConfigFolder.Contains(correctConfigFolderCheck))
+                {
+                    textBox1_luaLocation.Text = folderDlg.SelectedPath;
+                    fullPath_pluginsEnabledLua = (pathOfConfigFolder + @"\pluginsEnabled.lua");
+                    MessageBox.Show("Your new file will be exported as '" + fullPath_pluginsEnabledLua + "'. If that does not " +
+                        "look correct, please try again.");
+                }
+                else
+                {
+                    MessageBox.Show("It looks like you did not select the correct file. Please try again.");
+                    return;
+                }
+            }
+        }
+
+        private void label1_selectTheModules_Click(object sender, EventArgs e)
+        {
+            fullPath_pluginsEnabledLua = (@"C:\Users\Bailey\Saved Games\DCS.openbeta\Config\pluginsEnabled.lua");
+            textBox1_luaLocation.Text = fullPath_pluginsEnabledLua;
+        }
+
+        private void button3_deleteLua_Click(object sender, EventArgs e)
+        {
+            //https://stackoverflow.com/questions/3036829/how-do-i-create-a-message-box-with-yes-no-choices-and-a-dialogresult
+            string deleteLuaMessageText = ("Are you sure that you want to delete 'pluginsEnabled.lua'? Doing so will remove the file " +
+                "from your computer. If you delete this file you WILL be able to see or access all DCS modules.");
+            DialogResult dialogResult = MessageBox.Show(deleteLuaMessageText, "Are you sure?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //https://docs.microsoft.com/en-us/dotnet/api/system.io.file.delete?view=netcore-3.1
+                //do something
+                //check if file exists
+                //delete the file
+                if (File.Exists(fullPath_pluginsEnabledLua))
+                {
+                    File.Delete(fullPath_pluginsEnabledLua);
+                }
+                
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+                return;
+            }
+        }
+
+        private void button4_helpReadmee_Click(object sender, EventArgs e)
+        {
+            string helpReadmeMessage = ("Welcome to DCS Module Hider (DMoHi) v1. This program will enable you to select " +
+                "which aircraft you want to display or hide on the main menu of DCS (as well as the rest of DCS). " +
+                "It works for modules you have purchased, modules you have not purchased, and selected mods. This program " +
+                "modifies, creates, and deletes files on your computer. If you are not comfortable with that, do not " +
+                "use this utility." + "\r\n" + "\r\n" +
+                "1. Check the modules that you want to use and view in the DCS main menu. (I typically check modules " +
+                "that I own)." + "\r\n" + "\r\n" +
+                "2. Select your DCS Saved Games Config folder. It is likely located at " +
+                "‘C:\\Users\\YOURNAME\\SavedGames\\DCS.openbeta\\Config’." + "\r\n" + "\r\n" +
+                "3. Click the ‘Confirm and Export’ button. The utility will export a new ‘pluginsEnabled.lua’ to the chosen location. " +
+                " The utility will also create a folder and file where you ran the utility. This file contains the location of ‘" +
+                "pluginsEnabled.lua´ so you won’t have to search for it next time you use the utility." + "\r\n" + "\r\n" +
+                "4. If you want to unhide all modules in DCS press the ‘Unhide all modules’ button. Doing this will delete " +
+                "‘pluginsEnabled.lua’ and sets the visible DCS modules to default." + "\r\n" + "\r\n" +
+
+                "That’s it! Thank you for using DMoHi! If you have any questions, comments, concerns, or would just like" +
+                " to say “Thanks”, feel free to contact me via Discord: Bailey#6230." + "\r\n" + "\r\n" +
+
+                "Please feel free to donate. All donations go to making more free DCS utilities and mods for the community! " +
+                " https://www.paypal.com/paypalme/asherao" + "\r\n" + "\r\n" +
+
+                "If you would like to examine, follow, or add to DMoHi, the git is here: " +
+                "https://github.com/asherao" + "\r\n" + "\r\n" +
+
+                "Thank you to the Hoggit community on Discord for the idea and research for this utility." + "\r\n" +
+                "Enjoy!" + "\r\n" + "\r\n" +
+
+                "~Bailey" + "\r\n" +
+                "29AUG2020");
+
+            DialogResult dialogResult = MessageBox.Show(helpReadmeMessage, "Help / Readmee", MessageBoxButtons.OK);
+        }
+    }
+}
